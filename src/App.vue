739e3522813f36bc44f9d7254c4b9b0c5425e6c6
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-05-06 15:04:35 +0900

    Add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,17 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld />
-    </v-content>
+    <Toolbar />
   </v-app>
 </template>
 
 <script lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Toolbar from "./components/Toolbar.vue";
 
-export default {
-  name: "App",
+@Component({
   components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      //
-    };
+    Toolbar
   }
-};
+})
+export default class App extends Vue {}
 </script>
