439b5f8365b0ae3655023df69f3c516faa30477d
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-05-10 23:34:20 +0900

    Update GameManager

--- src/canvas-game/game-manager.ts
+++ src/canvas-game/game-manager.ts
@@ -8,15 +8,18 @@ import {
 } from "./actor-transformer";
 import { MechFigureActor } from "./mech-figure-actor";
 import { FrameActor } from "./frame-actor";
+import { GlowTextActor } from "./glow-text-actor";
 import logoImage from "@/assets/title-logo.png";
 import charImage from "@/assets/f066.png";
 import mechImage from "@/assets/f054.png";
 import backgroundImage from "@/assets/background_frame_set.png";
-import { GlowTextActor } from "./glow-text-actor";
-import { Vector } from "excalibur";
 
 export class GameManager {
   private game: ex.Engine;
+  private nameActor: GlowTextActor;
+  private charActor: FigureActor;
+  private mechActor: FigureActor;
+  private logoActor: FigureActor;
 
   constructor(canvas: HTMLCanvasElement) {
     const game = this.createGame(canvas);
@@ -30,6 +33,95 @@ export class GameManager {
     setupActorTransformer(trans, game);
 
     // Add frame
+    const [frame, frameTrans] = this.createFrameActor(canvasSize);
+    scene.add(frame);
+    frame.setZIndex(5);
+    const frameSize = new ex.Vector(frame.getWidth(), frame.getHeight());
+
+    // Add logo
+    this.logoActor = this.createFigureActor(
+      "logo",
+      { x: 0, y: 0.25 },
+      1,
+      logoImage,
+      frameSize,
+      frameTrans
+    );
+    scene.add(this.logoActor);
+    enableGrabbing(trans, this.logoActor);
+    this.logoActor.setZIndex(5);
+
+    // Add character name
+    this.nameActor = this.createCharacterNameActor(frameTrans);
+    scene.add(this.nameActor);
+    enableGrabbing(trans, this.nameActor);
+    this.nameActor.setZIndex(4);
+
+    // Add character
+    this.charActor = this.createFigureActor(
+      "char",
+      { x: -0.2, y: 0.125 },
+      1.25,
+      charImage,
+      frameSize,
+      frameTrans
+    );
+    scene.add(this.charActor);
+    enableGrabbing(trans, this.charActor);
+    this.charActor.setZIndex(3);
+
+    // Add background
+    const background = this.createFigureActor(
+      "background",
+      { x: 0, y: 0 },
+      1.25,
+      backgroundImage,
+      frameSize,
+      frameTrans
+    );
+    scene.add(background);
+    background.setZIndex(2);
+
+    // Add Mech
+    this.mechActor = this.createMechActor(canvasSize, frameSize, frameTrans);
+    scene.add(this.mechActor);
+    enableGrabbing(trans, this.mechActor);
+    this.mechActor.setZIndex(1);
+
+    this.game.addScene("main", scene);
+    this.game.start();
+    this.game.goToScene("main");
+  }
+
+  public updateCharacterName(name: string): void {}
+
+  public updateLogoFigure(figPath: string): void {}
+
+  public updateCharacterFigure(figPath: string): void {}
+
+  public updateMechFigure(figPath: string): void {}
+
+  private createGame(canvas: HTMLCanvasElement): ex.Engine {
+    // Override document.getElementById
+    // because ex.Engine use it but vue override it
+    const originalGetElementById = document.getElementById;
+    document.getElementById = () => canvas;
+
+    const game = new ex.Engine({
+      width: canvas.width,
+      height: canvas.height,
+      canvasElementId: "__anyId",
+      pointerScope: ex.Input.PointerScope.Canvas,
+      backgroundColor: ex.Color.DarkGray,
+      suppressConsoleBootMessage: true
+    });
+
+    document.getElementById = originalGetElementById;
+
+    return game;
+  }
+
+  private createFrameActor(canvasSize: ex.Vector): [FrameActor, mat.Matrix] {
     const nesicaSize = new ex.Vector(55, 85); // Nesica is 55 x 85 mm
     const canvasSizePerNecisa = new ex.Vector(
       canvasSize.x / nesicaSize.x,
@@ -50,67 +142,47 @@ export class GameManager {
       width: frameSize.x,
       height: frameSize.y
     });
-    scene.add(frame);
-    frame.setZIndex(5);
+    return [frame, frameTrans];
+  }
 
-    // Add logo
-    const logoPos = mat.applyToPoint(frameTrans, { x: 0, y: 0.25 });
-    const logo = new FigureActor({
-      name: "logo",
-      defaultFigurePath: logoImage,
-      initialSize: frameSize,
-      x: logoPos.x,
-      y: logoPos.y,
+  private createFigureActor(
+    name: string,
+    position: mat.Point,
+    initialSizeScale: number,
+    figurePath: string,
+    frameSize: ex.Vector,
+    frameTrans: mat.Matrix
+  ): FigureActor {
+    const pos = mat.applyToPoint(frameTrans, position);
+    return new FigureActor({
+      name,
+      defaultFigurePath: figurePath,
+      initialSize: frameSize.scale(initialSizeScale),
+      x: pos.x,
+      y: pos.y,
       width: 50,
       height: 50
     });
-    scene.add(logo);
-    enableGrabbing(trans, logo);
-    logo.setZIndex(5);
+  }
 
-    // Add character name
+  private createCharacterNameActor(frameTrans: mat.Matrix): GlowTextActor {
     const namePos = mat.applyToPoint(frameTrans, { x: -0.45, y: -0.125 });
-    const nameActor = new GlowTextActor("Asuha", {
+    return new GlowTextActor("Asuha", {
       x: namePos.x,
       y: namePos.y,
-      scale: new Vector(2, 2),
+      scale: new ex.Vector(2, 2),
       width: 50,
       height: 50
     });
-    scene.add(nameActor);
-    enableGrabbing(trans, nameActor);
-    nameActor.setZIndex(4);
+  }
 
-    // Add character
-    const charPos = mat.applyToPoint(frameTrans, { x: -0.2, y: 0.125 });
-    const char = new FigureActor({
-      name: "char",
-      defaultFigurePath: charImage,
-      initialSize: frameSize.scale(1.25),
-      x: charPos.x,
-      y: charPos.y,
-      width: 50,
-      height: 50
-    });
-    scene.add(char);
-    enableGrabbing(trans, char);
-    char.setZIndex(3);
-
-    // Add background
-    const backgroundPos = mat.applyToPoint(frameTrans, { x: 0, y: 0 });
-    const background = new FigureActor({
-      name: "background",
-      defaultFigurePath: backgroundImage,
-      initialSize: frameSize.scale(1.25),
-      x: backgroundPos.x,
-      y: backgroundPos.y
-    });
-    scene.add(background);
-    background.setZIndex(2);
-
-    // Add Mech
+  private createMechActor(
+    canvasSize: ex.Vector,
+    frameSize: ex.Vector,
+    frameTrans: mat.Matrix
+  ): MechFigureActor {
     const mechPos = mat.applyToPoint(frameTrans, { x: 0.25, y: 0.125 });
-    const mech = new MechFigureActor({
+    return new MechFigureActor({
       canvasSize,
       name: "mech",
       defaultFigurePath: mechImage,
@@ -121,32 +193,5 @@ export class GameManager {
       width: 50,
       height: 50
     });
-    scene.add(mech);
-    enableGrabbing(trans, mech);
-    mech.setZIndex(1);
-
-    this.game.addScene("main", scene);
-    this.game.start();
-    this.game.goToScene("main");
-  }
-
-  private createGame(canvas: HTMLCanvasElement): ex.Engine {
-    // Override document.getElementById
-    // because ex.Engine use it but vue override it
-    const originalGetElementById = document.getElementById;
-    document.getElementById = () => canvas;
-
-    const game = new ex.Engine({
-      width: canvas.width,
-      height: canvas.height,
-      canvasElementId: "__anyId",
-      pointerScope: ex.Input.PointerScope.Canvas,
-      backgroundColor: ex.Color.DarkGray,
-      suppressConsoleBootMessage: true
-    });
-
-    document.getElementById = originalGetElementById;
-
-    return game;
   }
 }
