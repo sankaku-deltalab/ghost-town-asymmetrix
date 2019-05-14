5f79dc8b39f58dd25cf790a65c53b30179795e48
Sankaku <sankaku_dlt.45631@outlook.jp>
2019-05-14 11:54:01 +0900

    Add ImageUploadButton

---
+++ src/components/ImageUploadButton.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <v-btn @click="pickFile">
+      <v-icon>attach_file</v-icon>
+      {{ this.name }}: {{ imageName }}
+    </v-btn>
+    <input
+      type="file"
+      ref="input"
+      accept="image/*"
+      hidden
+      @change="fileChanged"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Emit, Vue } from "vue-property-decorator";
+import { Promise } from "excalibur";
+
+@Component({})
+export default class ImageUploadButton extends Vue {
+  @Prop() readonly id!: string;
+  @Prop() readonly name!: string;
+  private imageName: string = "None";
+
+  private pickFile() {
+    (this.$refs.input as HTMLInputElement).click();
+  }
+
+  private fileChanged(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    if (target.files === null || target.files.length !== 1) {
+      alert("Select only one file");
+      return;
+    }
+    const imageFile = target.files[0];
+
+    // When file is not image, then warn
+    if (!imageFile.type.match("image.*")) {
+      alert("Select image file");
+      return;
+    }
+
+    this.loadImageURL(
+      imageFile,
+      (url: string): void => {
+        this.imageChange(this.id, url);
+      }
+    );
+  }
+
+  private loadImageURL(imageFile: File, onLoad: (url: string) => void): void {
+    // When file is not image, then warn
+    if (!imageFile.type.match("image.*")) {
+      alert("Select image file");
+      return;
+    }
+
+    // Read image and emit uploaded image url
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (typeof reader.result !== "string")
+        throw new Error("Image reading failed");
+      onLoad(reader.result);
+    };
+    reader.readAsDataURL(imageFile);
+  }
+
+  @Emit()
+  private imageChange(imageId: string, imageURL: string) {}
+}
+</script>
+
+<style scoped></style>
--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,57 +1,35 @@
 <template>
   <v-form>
-    <v-text-field
-      label="Character Image"
-      @click="pickFile"
-      v-model="characterImageName"
-      prepend-icon="attach_file"
-    ></v-text-field>
-    <input
-      type="file"
-      style="display: none"
-      ref="image"
-      accept="image/*"
-      @change="characterImageChanged"
+    <!-- Character name -->
+    <!-- color -->
+    <ImageUploadButton
+      v-for="info of buttonInfoArray"
+      v-bind:id="info.id"
+      v-bind:name="info.name"
+      v-on:image-change="imageChange"
+      v-bind:key="info.id"
     />
   </v-form>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Emit, Vue } from "vue-property-decorator";
+import ImageUploadButton from "./ImageUploadButton.vue";
 
-@Component({})
+@Component({
+  components: {
+    ImageUploadButton
+  }
+})
 export default class Settings extends Vue {
-  private characterImageName: string = "";
+  private buttonInfoArray: { id: string; name: string }[] = [
+    { id: "logo", name: "Title logo" },
+    { id: "character", name: "Character" },
+    { id: "mech", name: "Machine" }
+  ];
 
   private pickFile() {
     (this.$refs.image as HTMLInputElement).click();
-  }
-
-  private characterImageChanged(event: Event): void {
-    this.characterImageName = this.loadImage("character", event);
-  }
-
-  private loadImage(imageId: string, event: Event): string {
-    const target = event.target as HTMLInputElement;
-    if (target.files === null || target.files.length !== 1) return "";
-    const imageFile = target.files[0];
-
-    // When file is not image, then warn
-    if (!imageFile.type.match("image.*")) {
-      alert("Select image file");
-      return "";
-    }
-
-    // Read image and emit uploaded image url
-    const reader = new FileReader();
-    reader.onload = () => {
-      if (typeof reader.result !== "string")
-        throw new Error("Image reading failed");
-      this.imageChange(imageId, reader.result);
-    };
-    reader.readAsDataURL(imageFile);
-
-    return imageFile.name;
   }
 
   @Emit()
